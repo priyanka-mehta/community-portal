2f8b3c5782707a3e2f6c8a33f2ae4041b1403f96
Priyanka Mehta <priyankamehta929@gmail.com>
2022-07-25 15:35:53 +0530

    Issue resolved of hydration

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const [showChild, setShowChild] = useState(false);
+
+  useEffect(() => {
+    setShowChild(true)
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
   return <Component {...pageProps} />
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Community Portal</title>
         <meta name='description' content='Community Portal' />
         <link rel='icon' href='/favicon.ico' />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
           integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
       </Head>
       <body>
